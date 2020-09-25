f3268b1f5500bcd6a2d01b635607f42b83aaeba0
Иван Васильев <65816571+VasilevIvanVladimirovich@users.noreply.github.com>
2020-09-25 20:53:30 +0300

    Update main.cpp

--- lab-1/main.cpp
+++ lab-1/main.cpp
@@ -6,13 +6,13 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "rus");
-    Money M1(80,50);
-    Person P1;
-   //  M1.setBalance(50, 50);
-  //   M1.operationSum(30, 50);
+    Money M1(1,11);
+    Person P1(M1);
+    //M1.setBalance(99,99);
+    //M1.operationSum(9,9);
     try
     {
-   //     M1.operationMinus(90,10);
+        M1.operationMinus(90,10);
   //     M1.operationDivision(0);
     }
     catch (invalid_argument& e)
@@ -20,13 +20,12 @@ int main()
         cerr << e.what() << endl;
         return -1;
     }
- //   M1.operationDivision(3);
- //   if (M1.operationСomparison(30, 50)==0) cout << M1.getRub() << "," << M1.getKopeck() << " = " << 30 << "," << 50 << endl;
+ // M1.operationDivision(3);
+ // if (M1.operationСomparison(30, 50)==0) cout << M1.getRub() << "," << M1.getKopeck() << " = " << 30 << "," << 50 << endl;
 //  else if(M1.operationСomparison(30, 50)==1) cout << M1.getRub() << "," << M1.getKopeck() << " > " << 30 << "," << 50 << endl;
 //  else if(M1.operationСomparison(30, 50) == 2) cout << M1.getRub() << "," << M1.getKopeck() << " < " << 30 << "," << 50 << endl;
 
-//   P1.setBalance(M1.getRub(), M1.getKopeck());
- //  cout << "Balance = " << P1.getBalance() << endl;
+    P1.getBalance(M1);
     system("pause");
     return 0;
 }
