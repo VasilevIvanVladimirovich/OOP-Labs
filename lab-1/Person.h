03d6d82088c61147896adb7a5c6a5bec4debd9a0
Иван Васильев <65816571+VasilevIvanVladimirovich@users.noreply.github.com>
2020-10-06 22:50:14 +0300

    Update Person.h

--- lab-1/Person.h
+++ lab-1/Person.h
@@ -1,15 +1,30 @@
 #pragma once
 #include "Money.h"
+#include "Service.h"
 using namespace std;
 
 class Person {
 public:
 
-    Person(const Money balance);
+    Person(const Money& balance);
    
     ~Person();
     
-    void getBalance(const Money balance);
+    void Sum(int64_t rub, uint8_t kopeck);
+
+    void Minus(int64_t rub, uint8_t kopeck);
+
+    void Division(int64_t divider);
+
+    void Multiplication(int64_t multiplier);
+
+    int Сomparison(int64_t rub, uint8_t kopeck);
+
+    void getBalance();
+
+    int getKopeck();
+
+    int getRub();
    
 private:
     Money balance_;
