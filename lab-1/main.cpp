72c5e88e6d92997e513b6f21bcd6b379128f7b26
Иван Васильев <65816571+VasilevIvanVladimirovich@users.noreply.github.com>
2020-10-06 22:49:34 +0300

    Update main.cpp

--- lab-1/main.cpp
+++ lab-1/main.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
 #include "Money.h"
 #include "Person.h"
+#include "Service.h"
 using namespace std;
 
 int main()
 {
     setlocale(LC_ALL, "rus");
-    Money M1(1,11);
+    Money M1(5,60);
     Person P1(M1);
-    //M1.setBalance(99,99);
-    //M1.operationSum(9,9);
+    P1.Sum(5,51);
     try
     {
-        M1.operationMinus(90,10);
-  //     M1.operationDivision(0);
+       P1.Minus(4,20); 
+       //P1.Division(2);
     }
     catch (invalid_argument& e)
     {
         cerr << e.what() << endl;
         return -1;
     }
- // M1.operationDivision(3);
- // if (M1.operationСomparison(30, 50)==0) cout << M1.getRub() << "," << M1.getKopeck() << " = " << 30 << "," << 50 << endl;
-//  else if(M1.operationСomparison(30, 50)==1) cout << M1.getRub() << "," << M1.getKopeck() << " > " << 30 << "," << 50 << endl;
-//  else if(M1.operationСomparison(30, 50) == 2) cout << M1.getRub() << "," << M1.getKopeck() << " < " << 30 << "," << 50 << endl;
-
-    P1.getBalance(M1);
+   // if (P1.Сomparison(30, 50)==0) cout << P1.getRub() << "," << P1.getKopeck() << " = " << 30 << "," << 50 << endl;
+  //  else if(P1.Сomparison(30, 50)==1) cout << P1.getRub() << "," << P1.getKopeck() << " > " << 30 << "," << 50 << endl;
+  //  else if(P1.Сomparison(30, 50) == 2) cout << P1.getRub() << "," << P1.getKopeck() << " < " << 30 << "," << 50 << endl;
+    P1.getBalance();
     system("pause");
     return 0;
 }
