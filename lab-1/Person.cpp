d0e273b36d082ae8310598a6d0c613c0a3f182b6
Иван Васильев <65816571+VasilevIvanVladimirovich@users.noreply.github.com>
2020-10-06 22:50:50 +0300

    Update Person.cpp

--- lab-1/Person.cpp
+++ lab-1/Person.cpp
@@ -3,15 +3,49 @@
 #include "Money.h"
 using namespace std;
 
-Person::Person(const Money balance) :balance_(balance)
+Person::Person(const Money& balance) :balance_(balance)
 {
      cout << "Конструктор Person " << this << endl; 
 }
 
 Person::~Person() { cout << "деструктор Person " << this << endl; }
 
-void Person::getBalance(const Money balance)
+void Person::Sum(int64_t rub, uint8_t kopeck)
 {
-    this->balance_ = balance;
+    balance_.operationSum(rub, kopeck);
+}
+
+void Person::Minus(int64_t rub, uint8_t kopeck)
+{
+    balance_.operationMinus(rub, kopeck);
+}
+
+void Person::Division(int64_t divider)
+{
+    balance_.operationDivision(divider);
+}
+
+void Person::Multiplication(int64_t multiplier)
+{
+    balance_.operationMultiplication(multiplier);
+}
+
+int Person::getKopeck()
+{
+   return balance_.getKopeck();
+}
+
+int Person::getRub() 
+{
+    return balance_.getRub();
+}
+
+int Person::Сomparison(int64_t rub, uint8_t kopeck) 
+{
+    return balance_.operationСomparison(rub, kopeck);
+}
+
+void Person::getBalance()
+{
     cout << "Баланс = " << balance_.getRub() << "," << balance_.getKopeck() << endl;
 }
