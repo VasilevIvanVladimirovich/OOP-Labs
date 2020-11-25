a3cc1d4648631b31730388a7e527b1bba6213c62
Иван Васильев <65816571+VasilevIvanVladimirovich@users.noreply.github.com>
2020-11-25 10:10:09 +0300

    Update Subscriptions.cpp

--- lab-2/Subscriptions.cpp
+++ lab-2/Subscriptions.cpp
@@ -21,7 +21,7 @@ void Subscriptions::setPrice()
     std::cin>>r;
     std::cout<<"Enter Kopeck:";
     std::cin>>k;
-    setBalance(r,k);
+    price.setBalance(r,k);
     std::cout<<std::endl;
 }
 
@@ -57,7 +57,7 @@ void Subscriptions::setTime()
 }
 
 
-Subscriptions::Subscriptions(): Money(0,0)
+Subscriptions::Subscriptions():price(0,0)
 {
     name_subscriptions="-";
 }
@@ -72,8 +72,8 @@ std::string Subscriptions::getPrice()
     std::string price;
     if(key==1)
     {
-        return price="+"+std::to_string(getRub())+"."+std::to_string(getKopeck());
-    }else if(key==2) return price="-"+std::to_string(getRub())+"."+std::to_string(getKopeck());
+        return price="+"+std::to_string(this->price.getRub())+"."+std::to_string(this->price.getKopeck());
+    }else if(key==2) return price="-"+std::to_string(this->price.getRub())+"."+std::to_string(this->price.getKopeck());
 }
 
 void Subscriptions::printInfoSubscriptions()
@@ -100,15 +100,15 @@ int Subscriptions::balanceOperation(Person *P1)
     else if(frequency_sub==3) time.nextDay(360);
         if(key==1)
         {
-            P1->OperationSumMoney(getRub(),getKopeck());
+            P1->OperationSumMoney(price.getRub(),price.getKopeck());
             return 1;
         }else if(key==2)
         {
-            if(P1->OperationComparisonMoney(getRub(),getKopeck())==1)
+            if(P1->OperationComparisonMoney(price.getRub(),price.getKopeck())==1)
             {
-                P1->OperationMinusMoney(getRub(), getKopeck());
+                P1->OperationMinusMoney(price.getRub(), price.getKopeck());
                 return 1;
             }
-            else if(P1->OperationComparisonMoney(getRub(),getKopeck())==2) return 0;
-        }else P1->OperationMinusMoney(getRub(), getKopeck());
+            else if(P1->OperationComparisonMoney(price.getRub(),price.getKopeck())==2) return 0;
+        }else P1->OperationMinusMoney(price.getRub(), price.getKopeck());
 }
