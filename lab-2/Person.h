79260a9c6b2a98f7d6406800c77221dc161bf86f
Иван Васильев <65816571+VasilevIvanVladimirovich@users.noreply.github.com>
2020-12-05 12:41:00 +0300

    Update Person.h

--- lab-2/Person.h
+++ lab-2/Person.h
@@ -1,16 +1,20 @@
 #ifndef LAB_OOP_PERSON_H
 #define LAB_OOP_PERSON_H
 #pragma once
-#include "Money.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
 #include "stdint.h"
-
+#include "Subscriptions.h"
+#include "Money.h"
+#include "Person.h"
+#include "DateTime.h"
+#include <vector>
 
 class Person {
 public:
 
     Person(const Money& balance);
-
-    Person();
 
     ~Person();
 
@@ -30,12 +34,16 @@ public:
 
     int getRubBalanceMoney();
 
+    void setSubscriptionsPersons();
+
+    void PrintSubscriptions();
+
+    bool OperationSubscriptions(DateTime today_time);
+
 private:
     Money balance_;
+    int size_sub=0;
+    std::vector<Subscriptions> subscriptions_person;
 };
-
-
-
-
 
 #endif //LAB_OOP_PERSON_H
